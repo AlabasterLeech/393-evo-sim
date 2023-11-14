8e99843ccf496aa30761bbc4e14648324fb72bca
pgolnari2 <123515130+pgolnari2@users.noreply.github.com>
2023-11-13 23:22:17 -0500

    Add files via upload
    added object class

---
+++ src/Object.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Object:
+    def __init__(self, x, y, object_type, density):
+        self.x = x
+        self.y = y
+        self.object_type = object_type  # Type of object: 'food' or 'obstacle'
+        self.density = density  # Density of the object
+
+    def get_state(self):
+        # Returns the current state of the object
+        return {
+            "x": self.x,
+            "y": self.y,
+            "object_type": self.object_type,
+            "density": self.density
+        }
+
+    def set_state(self, state):
+        # Sets the state of the object
+        self.x = state["x"]
+        self.y = state["y"]
+        self.object_type = state["object_type"]
+        self.density = state["density"]
+

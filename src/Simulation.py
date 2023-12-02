5ea02878ce92ee6cc88e7864509909a4a56097fa
Bane-7 <taylorjgrossenbacher@gmail.com>
2023-12-01 19:40:59 -0500

    Basic framework for Simulation

---
+++ src/Simulation.py
@@ -0,0 +1,33 @@
+from Environment import Environment
+
+class Simulation:
+    def __init__(self, width, height):
+        self.create(width, height)
+    
+    def create(self, width, height):
+        #Initialize new simulation
+        self.env = Environment(width, height)
+        self.age = 0
+        self.gen = 0
+    
+    def load_json(self, filename):
+        #Load state from JSON
+        None
+
+    def save_json(self, filename):
+        #Save state to JSON
+        None
+    
+    def step(self):
+        #Calculate all organism behaviors and act on them
+        state = self.env.get_state()
+        for org in state["organisms"]:
+            org.think(self.env)
+        for org in state["organisms"]:
+            org.act(self.env)
+        self.age += 1
+    
+    def step_gen(self):
+        #Create new generation of organisms
+        self.age = 0
+        self.gen += 1

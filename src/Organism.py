e55019c62068dc2caf98f64a206c8982f5f690a1
CraftingBoss789 <jhx@case.edu>
2023-12-03 18:54:23 -0500

    Changed the actions for turn left/right

--- src/Organism.py
+++ src/Organism.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+
 class Organism:
     _NORTH = 0
     _EAST = 1
@@ -8,38 +9,38 @@ class Organism:
     _WEST = 3
 
     STIMULI = [
-        lambda organism, env: random.randrange(-2, 2)
-    ] * 64
+                  lambda organism, env: random.randrange(-2, 2)
+              ] * 64
     ACTIONS = [
-        lambda organism: organism.x, organism.y = [
-            (organism.x, organism.y - 1),
-            (organism.x + 1, organism.y),
-            (organism.x, organism.y + 1),
-            (organism.x - 1, organism.y)][organism.dir],
-        lambda organism: organism.x, organism.y = [
-            (organism.x, organism.y + 1),
-            (organism.x - 1, organism.y),
-            (organism.x, organism.y - 1),
-            (organism.x + 1, organism.y)][organism.dir],
-        lambda organism: organism.dir = (organism.dir - 1) % 4,
-        lambda organism: organism.dir = (organism.dir + 1) % 4,
-        lambda organism: None,
-        lambda organism: None
+    lambda organism: organism.x, organism.y = [
+        (organism.x, organism.y - 1),
+        (organism.x + 1, organism.y),
+        (organism.x, organism.y + 1),
+        (organism.x - 1, organism.y)][organism.dir],
+    lambda organism: organism.x, organism.y = [
+        (organism.x, organism.y + 1),
+        (organism.x - 1, organism.y),
+        (organism.x, organism.y - 1),
+        (organism.x + 1, organism.y)][organism.dir],
+    lambda organism: setattr(organism, 'dir', (organism.dir - 1) % 4),
+    lambda organism: setattr(organism, 'dir', (organism.dir + 1) % 4),
+    lambda organism: None,
+    lambda organism: None
     ]
     STIMULUS_WEIGHT = 2.
 
     def __init__(self, state):
-        #Initialize minimally functional default state
+        # Initialize minimally functional default state
         self.x, self.y = 0, 0
         self.dir = Organism._NORTH
         self.genome = []
         self.network = {}
 
-        #Copy initializer state into members
+        # Copy initializer state into members
         self.set_state(state)
-    
+
     def get_state(self):
-        #Return functional current state
+        # Return functional current state
         return {
             "x": self.x,
             "y": self.y,
@@ -48,47 +49,49 @@ class Organism:
         }
 
     def set_state(self, state):
-        #Copy initializer state into members
+        # Copy initializer state into members
         for attr in state:
             setattr(self, attr, state[attr])
 
     def think(self, env):
-        #Calculate neuron states
+        # Calculate neuron states
         for i in range(256):
             if i in self.network:
                 self.network[i].calc_output()
-    
+
     def act(self, env):
-        #Generate list of desired actions
+        # Generate list of desired actions
         actions = []
         for i in range(len(Organism.ACTIONS)):
-            if self.network[-i-1].get_output_thresh():
+            if self.network[-i - 1].get_output_thresh():
                 actions.append(i)
         actions = env.check(actions, self)
         for i in actions:
             Organism.ACTIONS[i](self)
 
     def consume(self, object):
-        #Modify values according to object composition
+        # Modify values according to object composition
         None
 
     def mutate(self, chance):
-        #Randomly alter singular gene in genome
+        # Randomly alter singular gene in genome
         if random.random() <= chance:
             target_gene = random.randrange(0, len(self.genome))
             target_byte = random.randrange(0, 3)
             new_byte = self.genome[target_gene][target_byte] ^ (1 << random.randrange(0, 8))
-            self.genome[target_gene] = self.genome[target_gene][:target_byte] + bytes([new_byte]) + self.genome[target_gene][target_byte + 1:]
-    
+            self.genome[target_gene] = self.genome[target_gene][:target_byte] + bytes([new_byte]) + self.genome[
+                                                                                                        target_gene][
+                                                                                                    target_byte + 1:]
+
     def build_network(self):
-        #Generate fresh neural network from stored genome
+        # Generate fresh neural network from stored genome
         self.network = {}
         for gene in self.genome:
-            #Extract neuron IDs and weight from gene
+            # Extract neuron IDs and weight from gene
             neuron_src = gene[0]
             neuron_dst = gene[2]
             weight = int.from_bytes([gene[1]], byteorder="little", signed=True) / 64.
-            #Skip if neurons are out of order
+            # Skip if neurons are out of order
             if neuron_dst < neuron_src:
                 continue
             elif neuron_dst == neuron_src:
@@ -96,33 +99,33 @@ class Organism:
                     self.network[neuron_src] = Organism.Neuron()
                 self.network[neuron_src].bias = weight
                 continue
-            #Create missing neurons
+            # Create missing neurons
             if neuron_src not in self.network:
                 self.network[neuron_src] = Organism.Neuron()
             if neuron_dst not in self.network:
                 self.network[neuron_dst] = Organism.Neuron()
-            #Create connection
+            # Create connection
             self.network[neuron_dst].add_input(self.network[neuron_src], weight)
-        #Connect stimuli to first several neurons
+        # Connect stimuli to first several neurons
         for i in range(len(Organism.STIMULI)):
             if i in self.network:
                 self.network[i].add_input(Organism.Stimulus(Organism.STIMULI[i], self), Organism.STIMULUS_WEIGHT)
 
     def get_genome(self):
-        #Return current genome
+        # Return current genome
         return self.genome
-    
+
     def set_genome(self, new_genome):
-        #Replace genome
+        # Replace genome
         self.genome = new_genome
-    
+
     def get_location(self):
-        #Return current location
+        # Return current location
         return (self.x, self.y)
 
     class Neuron:
         def __init__(self):
-            #Initialize basic neuron
+            # Initialize basic neuron
             self.output = 0
             self.bias = 0
             self.threshold = 0.6
@@ -130,47 +133,48 @@ class Organism:
             self.outputs = []
 
         def get_output(self):
-            #Return current activation output
+            # Return current activation output
             return self.output
 
         def get_output_thresh(self):
-            #Return if current output meets threshold
+            # Return if current output meets threshold
             return self.output >= self.threshold
 
         def add_input(self, neuron, weight):
-            #Add neuron to list of inputs with weight
+            # Add neuron to list of inputs with weight
             self.inputs.append((neuron, weight))
-            #Add this to target's list of outputs
+            # Add this to target's list of outputs
             neuron.add_output(self)
 
         def add_output(self, neuron):
-            #Add neuron to list of outputs
+            # Add neuron to list of outputs
             self.outputs.append(neuron)
-        
+
         def activation(self, signal):
-            #Calculate activation function
+            # Calculate activation function
             sigmoid = 1 / (1 + math.exp(-2 * signal))
             return math.tanh(2 * sigmoid - 1)
 
         def calc_output(self):
-            #Calculate activation output with current input
+            # Calculate activation output with current input
             signal = sum([neuron[0].get_output() * neuron[1] for neuron in self.inputs])
             signal += self.bias
             self.output = self.activation(signal)
-    
+
     class Stimulus:
         def __init__(self, stimulus, organism):
-            #Initialize a stimulus
+            # Initialize a stimulus
             self.output = stimulus
             self.organism = organism
-        
+
         def get_output(self):
-            #Return stimulus
+            # Return stimulus
             if self.output:
                 return self.output(self.organism)
             else:
                 return 0.
-        
+
         def add_output(self, _):
-            #Dummy function for compatibility
+            # Dummy function for compatibility
             None
+

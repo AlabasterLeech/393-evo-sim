20e5d627f0613bfe903600539252d4a72c2eec69
CraftingBoss789 <jhx@case.edu>
2023-12-04 00:35:06 -0500

    Added test cases for new params in init. Updated test_step

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -4,7 +4,15 @@ from src.Simulation import Simulation
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.sim = Simulation(10, 10)
+        self.width = 10
+        self.height = 10
+        self.pop = 3
+        self.survival = 10
+        self.age_max = 5
+        self.sim = Simulation(self.width, self.height, self.pop, self.survival, self.age_max)
+
+    def test_init(self):
+        self.assertEqual(self.pop, len(self.sim.env.organisms))
 
     def test_load_json(self):
         self.assertEqual(True, True)
@@ -16,6 +24,12 @@ class MyTestCase(unittest.TestCase):
         self.sim.step()
         msg = 'Age is not the same as expected age!'
         self.assertEqual(1, self.sim.age, msg)
+        self.sim.step()
+        self.sim.step()
+        self.sim.step()
+        self.sim.step()
+        self.assertEqual(0, self.sim.age, msg)
+        self.assertEqual(1, self.sim.gen, msg)
 
     def test_step_gen(self):
         self.sim.step_gen()

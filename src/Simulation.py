c03729e74c81779045fc099a448802e0a4b0dd09
Bane-7 <taylorjgrossenbacher@gmail.com>
2023-12-03 20:25:13 -0500

    Scatter newly created organisms

--- src/Simulation.py
+++ src/Simulation.py
@@ -16,12 +16,18 @@ class Simulation:
         self.mutation = 0.1
         #Create first generation with random genomes
         for _ in range(self.population):
-            patient = Organism({})
+            patient = Organism({
+                "x": random.randint(0, self.env.width - 1),
+                "y": random.randint(0, self.env.height - 1),
+                "dir": random.randint(0, 3)
+            })
             genome = []
             for __ in range(self.genome_length):
-                genome.append(bytes([random.randint(0,255), random.randint(0,255), random.randint(0,255)]))
+                genome.append(bytes([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]))
             patient.set_genome(genome)
             patient.build_network()
+            while not self.env.organisms.space_open(patient.x, patient.y):
+                patient.x, patient.y = random.randint(0, self.env.width - 1), random.randint(0, self.env.height - 1)
             self.env.organisms.append(patient)
     
     def load_json(self, filename):
@@ -54,6 +60,9 @@ class Simulation:
             offspring = self.env.breed(random.choice(parents, random.choice(parents)))
             offspring.mutate(self.mutation)
             offspring.build_network()
+            offspring.x, offspring.y = random.randint(0, self.env.width - 1), random.randint(0, self.env.height - 1)
+            while not self.env.organisms.space_open(offspring.x, offspring.y):
+                offspring.x, offspring.y = random.randint(0, self.env.width - 1), random.randint(0, self.env.height - 1)
             self.env.organisms.append(offspring)
         #Set counters
         self.age = 0

1c4c9c08ae4db93f49de7bfeb9741ae70ad7fb37
CraftingBoss789 <jhx@case.edu>
2023-12-04 16:20:26 -0500

    Added test cases for set_survival_function, load_json, save_json, auto_save methods.

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 import os
 import time
@@ -10,28 +11,59 @@ class SimulationTest(unittest.TestCase):
         self.width = 10
         self.height = 10
         self.pop = 3
+        self.food = 0.75
         self.survival = 10
         self.age_max = 5
-        self.sim = Simulation(self.width, self.height, self.pop, self.survival, self.age_max)
+        self.sim = Simulation(self.width, self.height, self.pop, self.food, self.survival, self.age_max)
+        self.survival_function = self.sim.survival_function
+        self.survival_function_name = self.sim.survival_function_name
 
     def test_init(self):
         self.assertEqual(self.pop, len(self.sim.env.organisms))
-        new_sim = Simulation(5, 5, 25, self.survival, self.age_max)
+        new_sim = Simulation(5, 5, 25, self.food, self.survival, self.age_max)
+        new_sim.step_gen()
 
     def test_load_json(self):
-        self.assertEqual(True, True)
+        filename = "test_load_json.json"
+        filepath = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", "..", "assets", filename))
+        flag = self.sim.load_json(filepath)
+        self.assertEqual(True, flag, 'File was not loaded!')
+        organisms = self.sim.env.get_organisms()
+        actual_location = organisms[0].get_location()
+        self.assertEqual((4, 1), actual_location, 'Not the same location!')
 
     def test_save_json(self):
-        self.assertEqual(True, True)
+        filename = "test_save_json.json"
+        filepath = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", "..", "assets", filename))
+        exp_orgs = 3
+        exp_max_age = 5
+        self.sim.save_json(filepath)
+        saved_file = open(filepath, "r")
+        saved_state = json.loads(saved_file.read())
+        saved_orgs = len(saved_state["env"]["organisms"])
+        saved_max_age = saved_state["age_max"]
+        msg = 'State was not saved properly!'
+        self.assertEqual(exp_orgs, saved_orgs, msg)
+        self.assertEqual(exp_max_age, saved_max_age, msg)
+        saved_file.close()
 
     def test_auto_save(self):
-        # DOES NOT WORK - PROBABLY BECAUSE NOT SAVING ANY DATA TO THE JSON FILE!!
+        # DOES NOT WORK
         self.sim.auto_save('filename')
-        current_time = time.strftime('%a-%b-%d-%H-%M-%S-%Y', time.localtime())
+        current_time = time.strftime('%a-%b-%e-%H-%M-%S-%Y', time.localtime()).replace(' ', '-')
         exp_file_name = f"AUTO-SAVE-{current_time}.json"
-        exp_file_path = os.path.join(os.path.abspath(__file__), "..", "..", "assets", exp_file_name)
+        exp_file_path = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", "..", "assets", exp_file_name))
         file_created = os.path.isfile(exp_file_path)
         self.assertTrue(file_created, "JSON file was not created!")
+
+    def test_set_survival_function(self):
+        # Check none survival function
+        self.assertEqual(self.survival, self.survival_function_name)
+        self.assertEqual(Simulation.SURVIVAL["None"], self.survival_function)
+
+        # Check setting actual survival function
+        sim_test = Simulation(self.width, self.height, self.pop, self.food, "North quarter", self.age_max)
+        self.assertEqual(Simulation.SURVIVAL["North quarter"], sim_test.survival_function)
 
     def test_step(self):
         self.sim.step()
